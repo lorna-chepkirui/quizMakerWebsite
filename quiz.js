c034871c7354168a0260a257457884ac8e7f39db
lorna-chepkirui <lornachepkirui20@gmail.com>
2022-05-19 08:25:10 +0300

    delete a var

--- quiz.js
+++ quiz.js
@@ -2,28 +2,28 @@ var questionBank = [
     {
     question: "Which of the following is not a HTML5 tag?",
     answer: "Slider",
-    options: ["track", "video", "slider", "source"]
+    option: ["track", "video", "slider", "source"]
   },
     {
     question: "An HTML document can contain?",
     answer: "All the answers are correct",
-    options: ["Attributes", "Tags", "Raw text", "All the answers are correct"]
+    option: ["Attributes", "Tags", "Raw text", "All the answers are correct"]
   },
     {
     question: "Suppose we want to arrange three DIVs so that DIV 3 is placed above DIV1. Now, which CSS property are we going to use to control the stack order??",
     answer: "z-index",
-    options: ["s-index", "z-index", "d-index", "x-index"]
+    option: ["s-index", "z-index", "d-index", "x-index"]
   },
     {
     question: "Can we define the direction of the text via a CSS property?",
     answer: "Yes its possible",
-    options: ["Yes it's possible", "No it's not possible"]
+    option: ["Yes it's possible", "No it's not possible"]
   },
     {
   
     question: "If we want to use a nice green dotted border around an image, which css property are we going to use??",
     answer: "border-style",
-    options: ["border-line", "border-style", "border-decoration"]
+    option: ["border-line", "border-style", "border-decoration"]
   },
 ];
 var question= document.getElementById('question');
